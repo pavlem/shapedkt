b93474b46ae5349286d96d0faea65b4d32162f46
Pavle Mijatović <paul.mijatovic@sprintingsoftware.com>
2019-08-09 17:40:59 +0200

    1

--- Sections/WeatherListCell.swift
+++ Sections/WeatherListCell.swift
@@ -23,19 +23,53 @@ class WeatherListCell: UITableViewCell {
     
     // MARK: - Properties
     // MARK: Outlets
-    @IBOutlet weak var wData: UILabel!
+    @IBOutlet weak var weatherInfoLbl: UILabel!
     // MARK: Constants
     static let id = "WeatherListCell_ID"
 
     // MARK: - Helper
     private func updateUI(withWeatherData weatherData: WeatherListData?) {
-        wData.text = weatherData?.wData
+        guard let inputString = weatherData?.wData else { return }
+        let attributedString = getAttributedString(inputString: inputString)
+        weatherInfoLbl.attributedText = attributedString
     }
     
     private func setUI() {
         selectionStyle = .none
-        wData.textColor = .white
-        wData.font = UIFont.systemFont(ofSize: 17)
+        weatherInfoLbl.textColor = .white
+        weatherInfoLbl.font = UIFont.systemFont(ofSize: 17)
+    }
+    
+    private func getAttributedString(inputString: String) -> NSMutableAttributedString? {
+        guard inputString.contains(":") else { return nil }
+        let stringArr = inputString.components(separatedBy: ":")
+        let firstPart = stringArr[0] + ":"
+        var secondPart = stringArr[1]
+        if stringArr.count == 3 {
+            secondPart.append(":")
+            secondPart.append(stringArr[2])
+        }
+        
+        let fontB = UIFont.boldSystemFont(ofSize: 19)
+        let fontR = UIFont.systemFont(ofSize: 17)
+        let txtColor = UIColor.white
+        
+        let attributesB: [NSAttributedString.Key: Any] = [
+            .font: fontB,
+            .foregroundColor: txtColor,
+        ]
+        
+        let attributesR: [NSAttributedString.Key: Any] = [
+            .font: fontR,
+            .foregroundColor: txtColor,
+        ]
+        
+        let firstPartAtt = NSAttributedString(string: firstPart, attributes: attributesR)
+        let secondPartAtt = NSAttributedString(string: secondPart, attributes: attributesB)
+        let completeString = NSMutableAttributedString()
+        completeString.append(firstPartAtt)
+        completeString.append(secondPartAtt)
+        return completeString
     }
     
     // MARK: - Lifecycle
